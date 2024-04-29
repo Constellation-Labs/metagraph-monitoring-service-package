d7f9cf25bff7e89d33bc2ca74522f8da103b2001
Marcus Girolneto <marcus.girolneto@gmail.com>
2024-04-29 12:26:28 -0300

    Updating kill current process command

--- src/monitor/restart/utils/kill-current-execution.ts
+++ src/monitor/restart/utils/kill-current-execution.ts
@@ -1,5 +1,5 @@
 import ISshService from '@interfaces/services/ssh/ISshService';
 
 export default async (sshService: ISshService, port: number): Promise<void> => {
-  await sshService.executeCommand(`kill -9 \`sudo lsof -t -i:${port}\``);
+  await sshService.executeCommand(`sudo fuser -k ${port}/tcp`);
 };
